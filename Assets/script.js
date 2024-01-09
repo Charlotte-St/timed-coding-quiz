ce1aeaa2a0647577df38e44c91f178660954f320
Lotte-Et <charlotte.stowe@gmail.com>
2024-01-09 18:47:27 -0500

    Added notes on structure of the Javascript file

---
+++ Assets/script.js
@@ -0,0 +1,20 @@
+//Add questions
+var questions = [
+    {
+        question: "What type of object can be stored in local storage?",
+        choices: ["a number", "a boolean", "a string", "a variable"],
+        answer: 3
+    },
+    {
+        question: "",
+        choices: [],
+        answer:
+    }
+];
+var highScores = {};
+var timeCount = 10;
+
+//Add timer 
+//Add quiz
+//Add button click event
+//Add interface for entering high score
