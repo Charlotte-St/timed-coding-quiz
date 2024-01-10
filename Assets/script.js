45865103f3518798d906164a7040aa7ff1e198c2
Lotte-Et <charlotte.stowe@gmail.com>
2024-01-09 19:13:50 -0500

    Added normalize.css; updated README; added some CSS

--- Assets/script.js
+++ Assets/script.js
@@ -6,15 +6,22 @@ var questions = [
         answer: 3
     },
     {
-        question: "",
-        choices: [],
-        answer:
+        question: "Which can be used to decrement a variable i by 1?",
+        choices: ["i==", "i--", "i++", "--i"],
+        answer: 2
     }
 ];
 var highScores = {};
 var timeCount = 10;
+var score = 0;
+
 
 //Add timer 
 //Add quiz
-//Add button click event
-//Add interface for entering high score
+// Update score as user takes quiz
+//Add button click eventt to launch quiz
+//Add interface for entering high score + name
+// Reset score after saving score + name to high scores
+// Add link to access high scores saved in local storage
+// Sort scores so highest score appears at the top
+// Add link to reset high scores
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# timed-coding-quiz
+# Timed Coding Quiz
+
+## Description
+
+This is a timed coding quiz. When the user finishes the quiz, they are allowed to enter their name in order to save their high score. 
+
+## Installation
+
+N/A
+
+## Usage
+
+## Credits
+
+[normalize.css v8.0.1 Copyright © Nicolas Gallagher and Jonathan Neal used under an MIT License](https://github.com/necolas/normalize.css)
+
+## License 
